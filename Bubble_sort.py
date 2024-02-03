e938ddf90969117b0cef815c18821a2161ecbc70
Hyeokie <fkgv123@gmail.com>
2024-02-03 02:09:56 +0100

    Add Bubble_sort.py and remove import requests.py

---
+++ Bubble_sort.py
@@ -0,0 +1,131 @@
+from re import L
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - i - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return arr
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] < arr[j - 1]:
+        arr[j], arr[j - 1] = arr[j - 1], arr[j]
+    return arr
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[i]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[i], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
+
+
+def Bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j + 1], arr[j] = arr[j], arr[j + 1]
+  return
+
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+print(Bubble_sort(arr))
